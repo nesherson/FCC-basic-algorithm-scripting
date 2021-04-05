0a5845f60924409970135e5244c1f67ebb8aff23
unknown <nesad.omeragich27@gmail.com>
2021-04-06 00:38:04 +0200

    Added tests for solution

--- Repeat a String Repeat a String/exercise.js
+++ Repeat a String Repeat a String/exercise.js
@@ -7,4 +7,13 @@ function repeatStringNumTimes(str, num) {
   }
 }
 
-repeatStringNumTimes('abc', 3);
+// TESTS
+// repeatStringNumTimes('abc', 3);
+// repeatStringNumTimes("*", 3)  ***.
+// repeatStringNumTimes("abc", 3)  abcabcabc.
+// repeatStringNumTimes("abc", 4)  abcabcabcabc.
+// repeatStringNumTimes("abc", 1)  abc.
+// repeatStringNumTimes("*", 8)  ********.
+// repeatStringNumTimes("abc", -2) should return an empty string ("").
+// repeatStringNumTimes("abc", 0) should return "".
+// The built-in repeat() method should not be used.
