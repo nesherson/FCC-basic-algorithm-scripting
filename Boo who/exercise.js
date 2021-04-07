960633850ed3ec03b28a7f24094a95cd5a0568b4
unknown <nesad.omeragich27@gmail.com>
2021-04-07 02:35:39 +0200

    Added tests for the solution

--- Boo who/exercise.js
+++ Boo who/exercise.js
@@ -1,5 +1,15 @@
 function booWho(bool) {
   return typeof bool === 'boolean' ? true : false;
 }
-
-booWho(null);
+//TESTS
+// booWho(null);
+// booWho(true) should return true.
+// booWho(false) should return true.
+// booWho([1, 2, 3]) should return false.
+// booWho([].slice) should return false.
+// booWho({ "a": 1 }) should return false.
+// booWho(1) should return false.
+// booWho(NaN) should return false.
+// booWho("a") should return false.
+// booWho("true") should return false.
+// booWho("false") should return false.
