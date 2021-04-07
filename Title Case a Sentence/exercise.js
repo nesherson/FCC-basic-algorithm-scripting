d5a7efc996e15e7d90955ed7196fc278189e7078
unknown <nesad.omeragich27@gmail.com>
2021-04-07 02:55:13 +0200

    Added tests for the solution

--- Title Case a Sentence/exercise.js
+++ Title Case a Sentence/exercise.js
@@ -1,9 +1,6 @@
 function titleCase(str) {
   const splitStr = str.toLowerCase().split(' ');
-  let titledStr = splitStr.map((word) => {
-    /*const titledLetter = word[0];
-      titledLetter.toUpperCase();*/
-
+  const titledStr = splitStr.map((word) => {
     const titledWord = word.replace(word[0], word[0].toUpperCase());
     return titledWord;
   });
@@ -11,3 +8,9 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+//TESTS
+// titleCase("I'm a little tea pot") should return a string.
+// titleCase("I'm a little tea pot") should return the string I'm A Little Tea Pot.
+// titleCase("sHoRt AnD sToUt") should return the string Short And Stout.
+// titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return the string Here Is My Handle Here Is My Spout.
