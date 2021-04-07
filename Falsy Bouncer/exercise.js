fe611c74dcc1c4a9d076ece01bb1331cbaf77872
unknown <nesad.omeragich27@gmail.com>
2021-04-07 23:50:14 +0200

    Added tests for the solution

--- Falsy Bouncer/exercise.js
+++ Falsy Bouncer/exercise.js
@@ -2,4 +2,8 @@ function bouncer(arr) {
   return arr.filter((item) => item && item);
 }
 
-bouncer([7, 'ate', '', false, 9]);
+// TESTS
+// bouncer([7, "ate", "", false, 9]) should return [7, "ate", 9].
+// bouncer(["a", "b", "c"]) should return ["a", "b", "c"].
+// bouncer([false, null, 0, NaN, undefined, ""]) should return [].
+// bouncer([null, NaN, 1, 2, undefined]) should return [1, 2].
