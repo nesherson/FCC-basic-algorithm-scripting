68cbf8404affe5cc1a273daad64f0f01dc376bef
unknown <nesad.omeragich27@gmail.com>
2021-04-07 23:44:42 +0200

    Added tests for the solution

--- Slice and Splice/exercise.js
+++ Slice and Splice/exercise.js
@@ -5,4 +5,11 @@ function frankenSplice(arr1, arr2, n) {
   return arr3;
 }
 
-frankenSplice([1, 2, 3], [4, 5, 6], 1);
+// TESTS
+// frankenSplice([1, 2, 3], [4, 5, 6], 1);
+// frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
+// frankenSplice([1, 2], ["a", "b"], 1) should return ["a", 1, 2, "b"].
+// frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2) should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"].
+// All elements from the first array should be added to the second array in their original order.
+// The first array should remain the same after the function runs.
+// The second array should remain the same after the function runs.
